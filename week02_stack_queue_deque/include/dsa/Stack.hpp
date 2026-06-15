14cb64440b23c5d547f8c9fdb4ad76b3dc427267
rezaguiamar <rezaguiamar11@gmail.com>
2026-06-16 00:56:30 +0200

    Update Stack.hpp

--- week02_stack_queue_deque/include/dsa/Stack.hpp
+++ week02_stack_queue_deque/include/dsa/Stack.hpp
@@ -1,27 +1,39 @@
-//
-// Created by marius on 1/26/26.
-//
-
 #pragma once
-#include <cstddef>
+#include "DynamicArray.hpp"
+#include <stdexcept>
 
 namespace dsa {
 
-    template <class T>
-    class Stack {
-    public:
-        Stack();
+template <class T>
+class Stack {
+public:
+    Stack() = default;
 
-        void push(const T& value);
-        void pop();
-        T& top();
-        const T& top() const;
+    void push(const T& value) {
+        data_.push_back(value);
+    }
 
-        bool empty() const;
-        std::size_t size() const;
+    void pop() {
+        if (empty()) throw std::out_of_range("Stack is empty");
+        data_.pop_back();
+    }
 
-    private:
-        // student decides representation
-    };
+    T& top() {
+        if (empty()) throw std::out_of_range("Stack is empty");
+        return data_.at(data_.size() - 1);
+    }
 
-}
+    const T& top() const {
+        if (empty()) throw std::out_of_range("Stack is empty");
+        return data_.at(data_.size() - 1);
+    }
+
+    bool empty() const { return data_.size() == 0; }
+
+    std::size_t size() const { return data_.size(); }
+
+private:
+    DynamicArray<T> data_;
+};
+
+} // namespace dsa
