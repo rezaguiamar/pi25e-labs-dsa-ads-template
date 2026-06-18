df3dfe4d5f5e0450bbf9ad0e217c2d529ccaff1b
rezaguiamar <rezaguiamar11@gmail.com>
2026-06-18 07:02:41 +0200

    Update DynamicArray.hpp

--- week01_dynamic_array/include/dsa/DynamicArray.hpp
+++ week01_dynamic_array/include/dsa/DynamicArray.hpp
@@ -15,37 +15,113 @@
 #include <cstddef>
 #include <stdexcept>
 
+
 namespace dsa {
 
-    template <class T>
-    class DynamicArray {
-    public:
-        DynamicArray();
-        ~DynamicArray();
+template <class T>
+class DynamicArray {
+public:
+   
+    DynamicArray() : data_(nullptr), size_(0), capacity_(0) {}
 
-        std::size_t size() const;
-        std::size_t capacity() const;
+    
+    ~DynamicArray() {
+        delete[] data_;
+    }
 
-        T& operator[](std::size_t index);
-        const T& operator[](std::size_t index) const;
+   
 
-        // Safe access
-        T& at(std::size_t index);
-        const T& at(std::size_t index) const;
 
-        void push_back(const T& value);
-        void pop_back();
+  
 
-        // Part B (lab / homework)
-        void insert(std::size_t index, const T& value);
-        void erase(std::size_t index);
+     
 
-        void reserve(std::size_t newCapacity);
+    
+    std::size_t size() const { return size_; }
+    std::size_t capacity() const { return capacity_; }
 
-    private:
-        T* data_;
-        std::size_t size_;
-        std::size_t capacity_;
-    };
+    
+    T& operator[](std::size_t index) { return data_[index]; }
+    const T& operator[](std::size_t index) const { return data_[index]; }
+
+   
+    T& at(std::size_t index) {
+        if (index >= size_)
+            throw std::out_of_range("Index out of bounds");
+        return data_[index];
+    }
+
+    const T& at(std::size_t index) const {
+        if (index >= size_)
+            throw std::out_of_range("Index out of bounds");
+        return data_[index];
+    }
+
+    void push_back(const T& value) {
+        if (size_ == capacity_)
+            reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+
+        data_[size_++] = value;
+    }
+
+    
+    void pop_back() {
+        if (size_ == 0)
+            throw std::out_of_range("Array is empty");
+        --size_;
+    }
+
+   
+    void insert(std::size_t index, const T& value) {
+        if (index > size_)
+            throw std::out_of_range("Index out of bounds");
+
+        if (size_ == capacity_)
+            reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+
+        for (std::size_t i = size_; i > index; --i)
+            data_[i] = data_[i - 1];
+
+        data_[index] = value;
+        ++size_;
+    }
+
+    
+    void erase(std::size_t index) {
+        if (index >= size_)
+            throw std::out_of_range("Index out of bounds");
+
+        for (std::size_t i = index; i < size_ - 1; ++i)
+            data_[i] = data_[i + 1];
+
+        --size_;
+    }
+
+   
+    void clear() {
+        size_ = 0;
+    }
+
+    
+    void reserve(std::size_t newCapacity) {
+        if (newCapacity <= capacity_)
+            return;
+
+        T* newData = new T[newCapacity];
+
+        for (std::size_t i = 0; i < size_; ++i)
+            newData[i] = data_[i];
+
+        delete[] data_;
+
+        data_ = newData;
+        capacity_ = newCapacity;
+    }
+
+private:
+    T* data_;
+    std::size_t size_;
+    std::size_t capacity_;
+};
 
 } // namespace dsa
